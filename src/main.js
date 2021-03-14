81b38f7f22a4871970ca5ecaf00a3afae06ecec1
Nicole Perkins <perkinsn@ufl.edu>
2021-03-14 16:19:50 -0500

    added js

---
+++ src/main.js
@@ -0,0 +1,23 @@
+class Desserts {
+    constructor(type, calories) {
+        this.type = type;
+        this.calories = calories;
+    }
+}
+
+class IceCream extends Desserts {
+    constructor(type, calories, flavor, scoops) {
+        super(type, calories);
+        this.flavor = flavor;
+        this.scoops = scoops;
+    }
+    includeSpoon() {
+        console.log('Here is your spoon!');
+    }
+}
+
+const vanillaIceCream = new IceCream('Ice Cream', 340, 'Vanilla', 3);
+
+console.log(vanillaIceCream);
+
+vanillaIceCream.includeSpoon();
